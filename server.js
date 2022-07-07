1564928d9c69f7c00bc556b525cce4562c7b18c8
vungnguyen <vungvovi@gmail.com>
2022-07-07 11:23:14 +0700

    ok

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('qs')
+
+const server = http.createServer(function (req, res) {
+    if (req.method === 'GET') {
+        fs.readFile('register.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
+        });
+    } else {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        })
+        req.on('end', () => {
+            const userInfo = qs.parse(data);
+            fs.readFile('info.html', 'utf8', function (err, datahtml) {
+                if (err) {
+                    console.log(err);
+                }
+                datahtml = datahtml.replace('{name}', userInfo.name);
+                datahtml = datahtml.replace('{email}', userInfo.email);
+                datahtml = datahtml.replace('{password}', userInfo.password);
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(datahtml);
+                return res.end();
+            });
+        })
+        req.on('error', () => {
+            console.log('error')
+        })
+    }
+});
+
+server.listen(8080, function () {
+    console.log('server running at localhost:8080 ')
+});
